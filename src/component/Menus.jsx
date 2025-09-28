32d521bfbc0eb82bb25cd3e62f4b6c91139582b9
manuhutu344 <nandoperdos2@gmail.com>
2025-09-28 22:23:33 +0900

    membuat koma di harga

--- src/component/Menus.jsx
+++ src/component/Menus.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
+import { Col } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import { numberWithCommas } from '../utils/utils';
 
 function Menus({menu}) {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Col md={4} xs={6} className='mb-4'>
+    <Card>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{menu.nama}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          Rp.{numberWithCommas(menu.harga)}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
+    </Col>
   )
 }
 
