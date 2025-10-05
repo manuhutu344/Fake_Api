41363ad81de2b56b02cea2c1468ceea28f70ea10
manuhutu344 <nandoperdos2@gmail.com>
2025-10-05 23:30:42 +0900

    menambahkan list nya dari react bostrap

--- src/component/ListCategories.jsx
+++ src/component/ListCategories.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { API_URL } from '../utils/constant'
 
-import React, { Component } from 'react'
-import { Col } from 'react-bootstrap'
+import { Component } from 'react'
+import { Col, ListGroup } from 'react-bootstrap'
 
 export default class ListCategories extends Component {
   constructor(props) {
@@ -29,6 +29,13 @@ componentDidMount(){
      <Col md={2} className='mt-2'>
       <h4><strong>Daftar Kategori</strong></h4>
       <hr />
+      <ListGroup>
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
      </Col>
     )
   }
