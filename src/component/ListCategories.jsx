9b0df4109833e02baccf93f009bb24390f9057a8
manuhutu344 <nandoperdos2@gmail.com>
2025-10-05 23:27:43 +0900

    console categories pertama

--- src/component/ListCategories.jsx
+++ src/component/ListCategories.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import axios from 'axios'
+import { API_URL } from '../utils/constant'
+
+import React, { Component } from 'react'
 import { Col } from 'react-bootstrap'
 
-export default function ListCategories() {
-  return (
-    <Col md={2} mt="2">
-        <h4>Daftar Kategori</h4>
-        <hr />
-    </Col>
-  )
+export default class ListCategories extends Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+     categories: []
+  }
+}
+
+componentDidMount(){
+    axios 
+    .get(API_URL + "categories")
+    .then((res)=>{
+      const categories = res.data
+      this.setState({categories})
+    })
+    .catch((error)=>{
+      console.log("Error yaa", error)
+    })
+  }
+  render() {
+    console.log(this.state.categories)
+    return (
+     <Col md={2} className='mt-2'>
+      <h4><strong>Daftar Kategori</strong></h4>
+      <hr />
+     </Col>
+    )
+  }
 }
